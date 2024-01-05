4b03c1012c42afb70b67488b088a6085898d8944
Nang Bui <nangbui3010@gmail.com>
2024-01-04 23:28:49 -0600

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Fatorizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxt = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.del = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(185, 127);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(389, 27);
+            this.txt.TabIndex = 0;
+            this.txt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(66, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rtxt
+            // 
+            this.rtxt.Location = new System.Drawing.Point(185, 210);
+            this.rtxt.Name = "rtxt";
+            this.rtxt.Size = new System.Drawing.Size(389, 120);
+            this.rtxt.TabIndex = 2;
+            this.rtxt.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(66, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Factorize";
+            // 
+            // del
+            // 
+            this.del.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.del.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.del.Location = new System.Drawing.Point(580, 125);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(94, 29);
+            this.del.TabIndex = 4;
+            this.del.Text = "DELETE";
+            this.del.UseVisualStyleBackColor = false;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-16, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(826, 540);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(167, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(424, 67);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Factorize Machine";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(217, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(344, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "This is the work of Nang Bui (John), Jake Pham.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Factorize machine";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fatorizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (txt.Text != "")
+            {
+                string s = "";
+                if (txt.Text.Length > 16)
+                {
+                    MessageBox.Show("Number is so large ! Literally unable to factorize", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txt.Focus();
+                    txt.SelectAll();
+                }
+                else
+                {
+                    long a = long.Parse(txt.Text);
+                    long i = 2;
+                    long[] ans = new long[10000];
+                    long c = 0;
+                    double t = (double)(a);
+                    long m = (long)(Math.Sqrt(t));
+                    while (i <= m)
+                    {
+                        while (a % i == 0)
+                        {
+                            a /= i;
+                            c++;
+                            if (c == 1) s += i.ToString();
+                            else s += '*' + i.ToString();
+
+                        }
+                        i++;
+                    }
+                    if (a > 1)
+                    {
+                        c++;
+                        if (c == 1) s += a.ToString();
+                        else s += '*' + a.ToString();
+
+                    }
+
+                    rtxt.Text = s;
+                }
+            }
+            else rtxt.Text = "";
+
+
+        }
+
+        private void txt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\b') e.Handled = false;
+            else if (txt.Text == "" && e.KeyChar == '0') e.Handled = true;
+            else if (e.KeyChar > '9' || e.KeyChar < '0') e.Handled = true;
+            else e.Handled = false;
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            txt.Text = "";
+            txt.Focus();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
